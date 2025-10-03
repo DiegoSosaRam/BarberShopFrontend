4891246646d6a1bdf7b9331f2d0a517d4cfb0b71
DiegoSosaRam <143459270+DiegoSosaRam@users.noreply.github.com>
2025-10-02 18:22:44 -0600

    creacion de la base del proyecto
    cada quien cree su rama dentro de pages/ para subir sus paginas. Una minimo

---
+++ BarberShop/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pages',
+    loadComponent: () => import('./pages/pages.page').then( m => m.PagesPage)
+  },
+];
